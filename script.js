cf7c0c10d245aa0bcf7ed3d78ca3868f854056a7
Mirade McGloin <mirade.mcgloin@gmail.com>
2020-06-29 17:09:29 -0600

    changing code to select password

--- script.js
+++ script.js
@@ -4,25 +4,47 @@ var lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 var uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 var numbers = '1234567890'.split('');
 var specialCharacters = '!"#$%&*(){}+,-./:;<=>?@[]^_`|~'.split('');
-var length = prompt ('How many characters would you like your password to include?') ;
-if(length<8 || length>128){
+/*if(length<8 || length>128){
   alert('Please enter a number between 8 and 128.');
+}*/
+
+var allArr = [];
+var passwordArr = [];
+function generatePassword () {
+var lengthOption = prompt ('How long do you want your password to be?'); 
+var upperOption = confirm ('Do you want to include Uppercase letters?');
+var lowerOption = confirm ('Do you want to use lowercase letters?');
+var numberOption = confirm ('Do you want to use numbers?')
+var specialOption = confirm ('Do you want to use special characters?');
+
+if (upperOption) {
+  allArr = allArr.concat(uppercaseLetters)
+}
+if (lowerOption) {
+  allArr = allArr.concat(lowercaseLetters)
+}
+if (numberOption) {
+  allArr = allArr.concat(numbers)
+}
+if (specialOption) {
+  allArr = allArr.concat(specialCharacters)
 }
 
-var password = {
-  lowercaseLetters : 'abcdefghijklmnopqrstuvwxyz'.split(''),
-  uppercaseLetters : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
-  numbers : '1234567890'.split(''),
-  specialCharacters : '!"#$%&*(){}+,-./:;<=>?@[]^_`|~'.split(''),
-  length : prompt ('How many characters would you like your password to include?') ;
-if(length<8 || length>128){
-  alert('Please enter a number between 8 and 128.');
-}
-  
-}
 
+
+for (var i = 0; i < lengthOption; i++) {
+  console.log(allArr);
+  //it will pick one random letter from allArr and push it to passwordArr  
+ var selectedChar = allArr[Math.floor(Math.random () * allArr.length)];
+ concat(selectedChar);
+ passwordArr.push(selectedChar);
+}
+return passwordArr;
+}
 // Write password to the #password input
 function writePassword() {
+
+ 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -30,12 +52,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-generateBtn.Click = confirm ('Do you want to include Uppercase letters?');
-generateBtn.Click = confirm ('Do you want to use lowercase letters?');
-generateBtn.Click = confirm ('Do you want to use numbers?')
-generateBtn.Click = confirm ('Do you want to use special characters?');
-generateBtn.Click = prompt ('How long do you want your password to be?'); 
+generateBtn.addEventListener('click', writePassword);
