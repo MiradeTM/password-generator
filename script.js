3baa035bc71c3842c9ab48f16b8b2bd39d899871
Mirade McGloin <mirade.mcgloin@gmail.com>
2020-06-28 21:39:51 -0600

    attempting some code

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+var uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+var numbers = '1234567890'.split('');
+var specialCharacters = '!"#$%&*(){}+,-./:;<=>?@[]^_`|~'.split('');
+var length = prompt ('How many characters would you like your password to include?') ;
+if(length<8 || length>128){
+  alert('Please enter a number between 8 and 128.');
+}
+
+var password = {
+  lowercaseLetters : 'abcdefghijklmnopqrstuvwxyz'.split(''),
+  uppercaseLetters : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+  numbers : '1234567890'.split(''),
+  specialCharacters : '!"#$%&*(){}+,-./:;<=>?@[]^_`|~'.split(''),
+  length : prompt ('How many characters would you like your password to include?') ;
+if(length<8 || length>128){
+  alert('Please enter a number between 8 and 128.');
+}
+  
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +32,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+generateBtn.Click = confirm ('Do you want to include Uppercase letters?');
+generateBtn.Click = confirm ('Do you want to use lowercase letters?');
+generateBtn.Click = confirm ('Do you want to use numbers?')
+generateBtn.Click = confirm ('Do you want to use special characters?');
+generateBtn.Click = prompt ('How long do you want your password to be?'); 
